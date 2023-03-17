76503a5abb2ab800dcce88eddf6055420d3bc1e6
Ajibola Sadiq <ajibola.sadiq@cognitivelabs.co>
2023-03-17 07:28:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+import { useEffect, useState } from 'react'
+
+
 
 function App() {
+  const socket = io.connect('http://localhost:8080')
+  const [input, setInput] = useState('')
+  const [message, setMessage] = useState()
+
+
+  function handleClick(){
+    socket.emit('sendMessage', {message: input})
+  }
+  function handleChange(e){
+    setInput(e.target.value)
+  }
+
+  useEffect(()=>{
+    socket.on('receiveMessage', (data)=>{
+      setMessage(data.message)
+      console.log(message)
+    })
+  }, [socket])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input placeholder='message' value={input} onChange={(e)=> handleChange(e)}/>
+        <button onClick={handleClick}>send message</button>
+      </form>
+      <h1>
+        Message
+      </h1>
+      <p>{message}</p>
     </div>
+    
   );
 }
 
